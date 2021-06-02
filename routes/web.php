8d15e40158ba5b703b7c25d8cd9d063d5edd620b
CrestNiraj12 <crestniraj@gmail.com>
2021-06-02 13:44:49 +0545

    updates

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 use TCG\Voyager\Facades\Voyager;
 
@@ -22,14 +23,56 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 
-Route::get('/courses/{path}', [CourseController::class, 'getCourseView']);
-Route::get('/blog/{path}', [BlogController::class, 'getBlogView']);
-Route::get('/news/{path}', [NewsController::class, 'getNewsView']);
-Route::get('/page/{path}', [PageController::class, 'getPageView']);
+Route::get('/', function () {
+    return view('/pages/landing-page');
+});
 
-Route::get( '/{path?}', function() {
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/courses/{slug}', [PagesController::class, 'getCourse']);
+    Route::get('/contact', [PagesController::class, 'getContact']);
+    Route::get('/enquiry', [PagesController::class, 'getEnquiry']);
+    Route::get('/applicationform', [PagesController::class, 'getApplication']);
+    Route::get('/registration-webinar', [PagesController::class, 'getRegistration']);
+    Route::get('/{slug}', [PagesController::class, 'getPage']);
+    /*
+    Route::get('/mdp', [PagesController::class, 'getMdp']);
+    Route::get('/csdp', [PagesController::class, 'getCsdp']);
+    Route::get('/fdp', [PagesController::class, 'getFdp']);
+    Route::get('/efg', [PagesController::class, 'getEfg']);
+    Route::get('/cds', [PagesController::class, 'getCds']);
+    Route::get('/banking-finance', [PagesController::class, 'getBankFinance']);
+    Route::get('/campus-life', [PagesController::class, 'getClife']);
+    Route::get('/gallery', [PagesController::class, 'getGallery']);
+
+
+ 
+    
+
+
+    // Courses Routes
+    Route::get('/pearson-hnd-hnc', [PagesController::class, 'getPearsonHNDHNC']);
+    Route::get('/tquk-hnd-hnc', [PagesController::class, 'getTQUKHNDHNC']);
+    // Route::get('/tquk-det', [PagesController::class, 'getTQUKDET']);
+    Route::get('/value-investing', [PagesController::class, 'getValueInvesting']);*/
+
+    // thank you pages
+
+    Route::get('/thankyou', [PagesController::class, 'getThankUs']);
+    Route::get('/enquiry-thankyou', [PagesController::class, 'getThanksUs']);
+    Route::get('/application-thankyou', [PagesController::class, 'getThankssUs']);
+});
+
+
+//Route::get('/courses/{path}', [CourseController::class, 'getCourseView']);
+//Route::get('/blog/{path}', [BlogController::class, 'getBlogView']);
+//Route::get('/news/{path}', [NewsController::class, 'getNewsView']);
+//Route::get('/page/{path}', [PageController::class, 'getPageView']);
+
+/*Route::get( '/{path?}', function() {
     return view( 'welcome' );
-} )->where('path', '.*');
+} )->where('path', '.*');*/
 
 
 
